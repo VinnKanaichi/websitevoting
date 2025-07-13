7f556026a3eae862be9ec1bafc1309669e1b5f0e
PutraOFC <115691638+VinnKanaichi@users.noreply.github.com>
2025-07-13 23:12:00 +0700

    Update firebase.ts

--- app/firebase.ts
+++ app/firebase.ts
@@ -1,15 +1,17 @@
 // app/firebase.ts
-import { initializeApp } from 'firebase/app'
+import { initializeApp } from "firebase/app"
+import { getDatabase } from "firebase/database"
 
 const firebaseConfig = {
   apiKey: "AIzaSyCxVtHwQlcmx_85nOz7nnqTlGOW_Hq1WF0",
   authDomain: "database-vot.firebaseapp.com",
   databaseURL: "https://database-vot-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "database-vot",
-  storageBucket: "database-vot.firebasestorage.app",
+  storageBucket: "database-vot.appspot.com",
   messagingSenderId: "455148726681",
   appId: "1:455148726681:web:89e91e7e2378bb836b4b08",
   measurementId: "G-Q53094PMYL"
 }
 
-export const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig)
+export const db = getDatabase(app)
